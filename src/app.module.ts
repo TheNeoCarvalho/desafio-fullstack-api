852ec967cfa4da535018e707c0fc03f0db06d6a9
Manoel Carvalho <manoel.carvalho@mediacaonline.com>
2021-12-13 21:34:40 -0300

    Install TypeOrm and env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+    imports: [TypeOrmModule.forRootAsync({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: async (configService: ConfigService) => ({
+            type: 'postgres',
+            host: 'postgres',
+            port: configService.get('POSTGRES_PORT', 5432),
+            username: configService.get('POSTGRES_USER', 'postgres'),
+            password: configService.get('POSTGRES_PASS', 'postgres'),
+            database: configService.get('POSTGRES_DATABASE', 'shoes'),
+            entities: [__dirname + '/**/*.entity{.ts,.js}'],
+            synchronize: true,
+        }),
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
