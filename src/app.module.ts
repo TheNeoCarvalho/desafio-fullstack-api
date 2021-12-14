820aae9ca90d80dd652315bfce9d5a193ba6b052
Manoel Carvalho <manoel.carvalho@mediacaonline.com>
2021-12-13 21:39:42 -0300

    Doc with Swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+import { OrderModule } from './order/order.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-    imports: [TypeOrmModule.forRootAsync({
+    imports: [ProductModule, OrderModule, TypeOrmModule.forRootAsync({
         imports: [ConfigModule],
         inject: [ConfigService],
         useFactory: async (configService: ConfigService) => ({
             type: 'postgres',
-            host: 'postgres',
+            host: configService.get('POSTGRES_HOST', 'postgres'),
             port: configService.get('POSTGRES_PORT', 5432),
             username: configService.get('POSTGRES_USER', 'postgres'),
             password: configService.get('POSTGRES_PASS', 'postgres'),
